4e0371c479b75e0834b465f53486e3052b7b6da6
sam <116354478+srslopes@users.noreply.github.com>
2024-05-16 19:43:00 -0300

    Ajustes para hospedagem

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
+import Link from 'next/link';
 import {useEffect, useState} from 'react';
 
 import {ControleSeries} from '@/controle/controle.series';
@@ -69,6 +70,11 @@ export default function Home() {
     }
     function handleremover(id: number) {
         setRemovida(id);
+    }
+    function search(event: any) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+        }
     }
 
     return (
@@ -323,28 +329,21 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className='absolute top-0 right-0 m-4 flex items-center ps-3 cursor-pointer'>
-                <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='16'
-                    height='16'
-                    fill='currentColor'
-                    className='bi bi-question-circle'
-                    viewBox='0 0 16 16'
-                    onClick={() => sobre()}
-                >
-                    <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16' />
-                    <path d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94' />
-                </svg>
-            </div>
+            <Link href={'/sobre'}>
+                <div className='absolute top-0 right-0 m-4 flex items-center ps-3 cursor-pointer'>
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='16'
+                        height='16'
+                        fill='currentColor'
+                        className='bi bi-question-circle'
+                        viewBox='0 0 16 16'
+                    >
+                        <path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16' />
+                        <path d='M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94' />
+                    </svg>
+                </div>
+            </Link>
         </div>
     );
-}
-
-export function sobre() {}
-
-export function search(event: any) {
-    if (event.keyCode == 13) {
-        event.preventDefault();
-    }
 }
--- app/sobre/page.tsx
+++ app/sobre/page.tsx
@@ -1,3 +1,68 @@
+/* eslint-disable react/no-unescaped-entities */
+import Link from 'next/link';
+
 export default function Home() {
-    return <div></div>;
+    return (
+        <div>
+            <Link href={'/'}>
+                <div className='absolute top-0 left-0 m-4 flex items-center ps-3 cursor-pointer'>
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        width='16'
+                        height='16'
+                        fill='currentColor'
+                        className='bi bi-arrow-left-circle'
+                        viewBox='0 0 16 16'
+                    >
+                        <path
+                            fill-rule='evenodd'
+                            d='M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z'
+                        />
+                    </svg>
+                </div>
+            </Link>
+            <div className='m-20 align-middle justify-center flex flex-col'>
+                <label className=''>CALCULADORA DE SÉRIES</label>
+                <label>
+                    Calcule quanto tempo da sua vida foi gasto assistindo
+                    series.
+                </label>
+                <label>
+                    Digite o nome da série desejada para o sistema procurá-la na
+                    base de dados, selecione entre as opções adequadas para
+                    adicioná-la a sua lista.
+                </label>
+                <label>
+                    Digite o nome da série desejada para o sistema procurá-la na
+                    base de dados, selecione entre as opções adequadas para
+                    adicioná-la a sua lista.
+                </label>
+                <label>
+                    Para cada série adicionada ou removida o total de tempo
+                    assistido é atualizado.
+                </label>
+                <label>
+                    Clique no botão "Remover" no card da série caso deseje
+                    excluí-la da lista.
+                </label>
+                <label>
+                    Projeto desenvolvido para teste técnico de uma vaga de
+                    estágio.
+                </label>
+                <label>
+                    O sistema usa a base de dados The Movie Database(TMDB).
+                </label>
+                <label>
+                    O objetivo é demonstrar a aptidão no uso de tecnologias como
+                    NEXTjs, Tailwind CSS, chamadas de API, etc.
+                </label>
+                <label>Link github:</label>
+                <Link href={'https://github.com/srslopes/CalculadoraSeries'}>
+                    <label className='text-blue-500 cursor-pointer hover:text-blue-300'>
+                        https://github.com/srslopes/CalculadoraSeries
+                    </label>
+                </Link>
+            </div>
+        </div>
+    );
 }
